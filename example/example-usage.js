06400a244188ac1f5abfb15b5cf924c6b63f68cf
Taka Okunishi <okunishinishi@gmail.com>
2016-10-30 17:40:39 +0900

    Version incremented to 1.0.1

--- example/example-usage.js
+++ example/example-usage.js
@@ -1,3 +1,17 @@
 'use strict'
 
-const akoaOauth = require('akoa-oauth')
+const co = require('co')
+const aoauth = require('aoauth')
+
+co(function * () {
+  let server = aoauth({
+    twitter: aoauth.twitterStrategy(
+      'your-consumer-key',
+      'your-secret',
+      'http://localhost:3000/oauth/twitter/callback'
+    )
+  }, {
+    prefix: '/oauth'
+  })
+  server.listen(3010)
+}).catch((err) => console.error(err))
